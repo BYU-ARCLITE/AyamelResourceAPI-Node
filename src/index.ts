be9d344169a41b5e55e130ece8401170a83ce914
Logan Kearsley <chronosurfer@gmail.com>
2021-02-18 11:35:50 -0700

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,111 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient, ObjectID } from 'mongodb';
+import fs from 'fs';
+
+const { db_uri, db_name, server_port } =
+  JSON.parse(fs.readFileSync('../config.json', 'utf8'));
+
+const DBPromise = MongoClient.connect(db_uri)
+  .then(client => client.db(db_name));
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/api/v1/docs', (_, res) => {
+  res.send('Hello World');
+});
+
+/* RESOURCE ROUTES */
+
+app.get('/api/v1/resources', async (_, res) => {
+  const db = await DBPromise;;
+  res.send(JSON.stringify({
+    status: 200,
+    ids: await db.collection('resources').distinct('_id'),
+  }));
+});
+
+app.get('/api/v1/resources/search', async (_, res) => {
+  //const db = await DBPromise;
+  res.status(501);
+  res.end(JSON.stringify('{"status":501}'));
+});
+
+app.get('/api/v1/resources/scan', async (_, res) => {
+  //const db = await DBPromise;
+  res.status(501);
+  res.end(JSON.stringify('{"status":501}'));
+});
+
+/* Resource Life Cycle Routes */
+app.post('/api/v1/resources', async (req, res) => {
+  const db = await DBPromise;
+  const { insertedId } = await db.collection('resources').
+    insertOne(req.body);
+  res.status(201);
+  res.send(JSON.stringify({ status: 201, id: insertedId }));
+});
+
+app.put('/api/v1/resources/:id', async (_, res) => {
+  //const db = await DBPromise;
+  res.status(501);
+  res.end(JSON.stringify('{"status":501}'));
+});
+
+app.get('/api/v1/resources/:id', async (req, res) => {
+  // TODO: retrieve relations
+  const db = await DBPromise;
+  const doc = await db.collection('resources')
+    .findOne({"_id": new ObjectID(req.params.id)});
+  res.status(200);
+  res.send(JSON.stringify({ status: 200, resource: doc }));
+});
+
+app.delete('/api/v1/resources/:id', async (req, res) => {
+  const db = await DBPromise;
+  await db.collection('resources')
+    .deleteOne({"_id": new ObjectID(req.params.id)});
+  res.status(204);
+  res.send('{"status":204}');
+});
+
+/* RELATION ROUTES */
+
+app.get('/api/v1/relations', async (_, res) => {
+  const db = await DBPromise;;
+  res.send(JSON.stringify({
+    status: 200,
+    ids: await db.collection('relations').distinct('_id'),
+  }));
+});
+
+/* Relation Life Cycle Routes */
+
+app.post('/api/v1/relations', async (req, res) => {
+  const db = await DBPromise;
+  const { insertedId } = await db.collection('relations').
+    insertOne(req.body);
+  res.status(201);
+  res.send(JSON.stringify({ status: 201, id: insertedId }));
+});
+
+app.get('/api/v1/relations/:id', async (req, res) => {
+  // TODO: retrieve relations
+  const db = await DBPromise;
+  const doc = await db.collection('relations')
+    .findOne({"_id": new ObjectID(req.params.id)});
+  res.status(200);
+  res.send(JSON.stringify({ status: 200, resource: doc }));
+});
+
+app.delete('/api/v1/relations/:id', async (req, res) => {
+  const db = await DBPromise;
+  await db.collection('relations')
+    .deleteOne({"_id": new ObjectID(req.params.id)});
+  res.status(204);
+  res.send('{"status":204}');
+});
+
+app.listen(server_port);
